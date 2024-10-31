bb5e3e1e1bbde04ec08ad72ee5e78ec3d2785b74
Andreas Meier <andreas.meier@ethz.ch>
2024-10-31 16:06:37 +0100

    Make minimal example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,32 @@
 package org.example;
 
+import org.apache.jena.ontology.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main
 {
+
+    public static final String EXAMPLE_TTL = "example.ttl";
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+
+        OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+        RDFDataMgr.read(ontModel, EXAMPLE_TTL, Lang.TTL);
+        List<OntProperty> properties = new ArrayList<>();
+        List<OntClass> classes = new ArrayList<>();
+
+        ontModel.listOntProperties().forEach(properties::add);
+        ontModel.listClasses().forEach(classes::add);
+        Restriction restriction =
+                classes.stream().filter(x -> x.isRestriction()).toList().get(0).asRestriction();
+        OntProperty failing = restriction.getOnProperty().asProperty();
+
     }
 }
